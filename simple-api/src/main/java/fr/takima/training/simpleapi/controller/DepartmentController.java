b6641e81ba1573bbf3a38fba36ffc9a29e49ccce
schrk06 <scharikh0@gmail.com>
2026-04-23 01:07:12 +0200

    test pc

--- simple-api/src/main/java/fr/takima/training/simpleapi/controller/DepartmentController.java
+++ simple-api/src/main/java/fr/takima/training/simpleapi/controller/DepartmentController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-
+//test
 @RestController
 @CrossOrigin
 @RequestMapping(value = "/departments")
